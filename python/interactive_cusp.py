dd1ead416da76671de8b0aebbe4aaa22f0962863
Nick Shaw <nick@antlerpost.com>
2023-01-19 14:47:09 +0000

    Interactive cusp uses J_resolution

--- python/interactive_cusp.py
+++ python/interactive_cusp.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider
 import cusp_path
+from cusp_path import J_resolution
 
 fig, ax = plt.subplots(figsize=(10,10) )
 plt.subplots_adjust(left=0.05, top=0.9, bottom=0.3, right=0.97)
@@ -13,7 +14,7 @@ plt.axis([0, 100, 0, 100])
 hue_slider = plt.axes([0.05, 0.1, 0.2, 0.01])
 h = Slider(hue_slider, 'h', 0, 360, valinit=0, valfmt="%1.1f")
 
-J = np.linspace(0, 100, 256)
+J = np.linspace(0, 100, J_resolution)
 M = cusp_path.find_boundary(h.val)
 
 curve, = ax.plot( M, J, color='blue')
