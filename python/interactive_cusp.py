2f474388139da81329667e0bb70bad1df05d5f0d
Pekka Riikonen <priikone@iki.fi>
2023-01-20 02:32:57 +0200

    Add CAM DRT gamut mapper
    Adds both iterative gamut intersection finding and approximation
    to triangle intersection.
    
    Plots both intersection (red dot) and the compressed color.  When
    approximation is enabled plots also the triangle (red line).

--- python/interactive_cusp.py
+++ python/interactive_cusp.py
@@ -3,8 +3,22 @@ import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.widgets import Slider, CheckButtons
 import cusp_path
-from cusp_path import J_resolution, PLOT_COLOURSPACE, Hellwig2022_to_XYZ, CAM_Specification_Hellwig2022
-
+from cusp_path import J_resolution, PLOT_COLOURSPACE, Hellwig2022_to_XYZ, CAM_Specification_Hellwig2022, forwardGamutMapper
+from colour.utilities import (
+    CanonicalMapping,
+    MixinDataclassArithmetic,
+    as_float,
+    as_float_array,
+    from_range_100,
+    from_range_degrees,
+    full,
+    has_only_nan,
+    ones,
+    to_domain_100,
+    to_domain_degrees,
+    tsplit,
+    tstack,
+)
 XYZ_w = colour.xy_to_XYZ(PLOT_COLOURSPACE.whitepoint) * 100
 L_A = 100.0
 Y_b = 20.0
@@ -18,58 +32,48 @@ def JMh_to_RGB(J, M, h):
     RGB = np.clip(RGB, 0, 1)**(1/2.2)
     return (RGB[0], RGB[1], RGB[2])
 
-def compress(dist, lim=1.6, thr=0.75, power=1.2, invert=False):
-    # power(p) compression function plot https://www.desmos.com/calculator/54aytu7hek
-    s = (lim-thr)/np.power(np.power((1-thr)/(lim-thr),-power)-1,1/power) # calc y=1 intersect
-    if not invert:
-	    cdist = thr+s*((dist-thr)/s)/(np.power(1+np.power((dist-thr)/s,power),1/power)) # compress
-    else:
-	    cdist = thr+s*np.power(-(np.power((dist-thr)/s,power)/(np.power((dist-thr)/s,power)-1)),1/power) # uncompress
-
-    cdist = np.nan_to_num(cdist)
-
-    cdist[dist < thr] = dist[dist < thr]
-
-    return cdist
-
-def gamut_compress(J, M, M_bound):
-    M_thresh = M_bound[int(J_resolution * J / 100.0)]
-    M_norm = M / M_thresh
-    M_comp = compress(np.array([M_norm]))[0]
-    return J, M_comp * M_thresh
-
 J_range = np.linspace(0, 100, J_resolution)
 
 fig, ax = plt.subplots(figsize=(10,10) )
 plt.subplots_adjust(left=0.05, top=0.9, bottom=0.3, right=0.97)
 
 plt.axis([0, 100, 0, 100])
-plt.title("Simple M only Gamut Compression")
+plt.title("CAM DRT Gamut Mapping")
 
 hue_slider = plt.axes([0.05, 0.1, 0.2, 0.01])
-h = Slider(hue_slider, 'h', 0, 360, valinit=180, valfmt="%1.1f")
+h = Slider(hue_slider, 'h', 0, 360, valinit=30, valfmt="%1.1f")
 
 source_M = plt.axes([0.05, 0.15, 0.2, 0.01])
-SM = Slider(source_M, 'M', 0, 100, valinit=50, valfmt="%1.1f")
+SM = Slider(source_M, 'M', 0, 100, valinit=80, valfmt="%1.1f")
 
 source_J = plt.axes([0.05, 0.2, 0.2, 0.01])
-SJ = Slider(source_J, 'J', 0, 100, valinit=50, valfmt="%1.1f")
+SJ = Slider(source_J, 'J', 0, 100, valinit=30, valfmt="%1.1f")
+
+check_box = plt.axes([0.8, 0.1, 0.16, 0.12])
+check_boxes = CheckButtons(check_box, ['Show Cusp', 'Show Path', 'Approximation'], [1, 1, 1])
 
 M_bound = cusp_path.find_boundary(h.val)
-CJ, CM = gamut_compress(SJ.val, SM.val, M_bound)
+M_cusp = M_bound.max()
+J_cusp = 100.0 * M_bound.argmax() / (J_resolution - 1)
+compr = forwardGamutMapper(np.array([SJ.val, SM.val, h.val]), np.array([J_cusp, M_cusp]),
+                           check_boxes.get_status()[2] == 1)
 
+CJ, CM, hue, focusJ, ixJ, ixM = tsplit(compr);
 RGB = JMh_to_RGB(CJ, CM, h.val)
 compressed, = ax.plot(CM, CJ, color=RGB, marker='o')
-
-check_box = plt.axes([0.85, 0.1, 0.12, 0.12])
-check_boxes = CheckButtons(check_box, ['Show Cusp', 'Show Path'], [1, 1])
+ix, = ax.plot(ixM, ixJ, color="red", marker='o')
+focus, = ax.plot(0, focusJ, color="gray", marker='o')
 
 RGB = JMh_to_RGB(SJ.val, SM.val, h.val)
 source, = ax.plot(SM.val, SJ.val, color=RGB, marker='o')
 
 curve, = ax.plot( M_bound, J_range, color='blue')
 
-comp_label = ax.text(36, 30, "Compressed:\n  J = {:.1f}\n  M = {:.1f}".format(CJ, CM))
+comp_label = ax.text(80, 5,
+"Intersection:\n  J = {:.1f}\n  M = {:.1f}\n\n"
+"Compressed:\n  J = {:.1f}\n  M = {:.1f}"
+.format(ixJ, ixM, CJ, CM)
+)
 
 if check_boxes.get_status()[0]==1:
     M_cusp = M_bound.max()
@@ -79,6 +83,12 @@ if check_boxes.get_status()[0]==1:
 
 if check_boxes.get_status()[1]==1:
     path, = ax.plot([SM.val, CM], [SJ.val, CJ], color='black')
+    pathix, = ax.plot([SM.val, ixM], [SJ.val, ixJ], color='black')
+    pathix0, = ax.plot([ixM, 0], [ixJ, focusJ], color='black')
+    if check_boxes.get_status()[2] == 1:
+        ixl0, = ax.plot([0, M_cusp], [0, J_cusp], color='red')
+        ixl1, = ax.plot([0, M_cusp], [100, J_cusp], color='red')
+
 
 def update(val):
     M_bound = cusp_path.find_boundary(h.val)
@@ -86,18 +96,28 @@ def update(val):
     J_cusp = 100.0 * M_bound.argmax() / (J_resolution - 1)
     curve.set_xdata( M_bound )
     curve.set_ydata( J_range )
-    CJ, CM = gamut_compress(SJ.val, SM.val, M_bound)
+
+    compr = forwardGamutMapper(np.array([SJ.val, SM.val, h.val]), np.array([J_cusp, M_cusp]),
+                               check_boxes.get_status()[2] == 1)
+    CJ, CM, hue, focusJ, ixJ, ixM = tsplit(compr);
     RGB = JMh_to_RGB(CJ, CM, h.val)
     compressed.set_xdata(CM)
     compressed.set_ydata(CJ)
     compressed.set_color(RGB)
+    ix.set_xdata(ixM)
+    ix.set_ydata(ixJ)
+    focus.set_xdata(0)
+    focus.set_ydata(focusJ)
+
     RGB = JMh_to_RGB(SJ.val, SM.val, h.val)
     source.set_xdata(SM.val)
     source.set_ydata(SJ.val)
     source.set_color(RGB)
-    comp_label.set_text("Compressed:\n  J = {:.1f}\n  M = {:.1f}".format(CJ, CM))
-    comp_label.set_x(CM)
-    comp_label.set_y(CJ - 20)
+    comp_label.set_text(
+"Intersection:\n  J = {:.1f}\n  M = {:.1f}\n\n"
+"Compressed:\n  J = {:.1f}\n  M = {:.1f}"
+.format(ixJ, ixM, CJ, CM)
+)
     if check_boxes.get_status()[0]==1:
         RGB = JMh_to_RGB(J_cusp, M_cusp, h.val)
         cusp.set_xdata(M_cusp)
@@ -108,6 +128,20 @@ def update(val):
     if check_boxes.get_status()[1]==1:
         path.set_xdata([SM.val, CM])
         path.set_ydata([SJ.val, CJ])
+        pathix.set_xdata([SM.val, ixM])
+        pathix.set_ydata([SJ.val, ixJ])
+        pathix0.set_xdata([ixM, 0])
+        pathix0.set_ydata([ixJ, focusJ])
+        if check_boxes.get_status()[2] == 1:
+            ixl0.set_xdata([0, M_cusp])
+            ixl0.set_ydata([0, J_cusp])
+            ixl1.set_xdata([0, M_cusp])
+            ixl1.set_ydata([100, J_cusp])
+        else:
+            ixl0.set_xdata([0, 0])
+            ixl0.set_ydata([0, 0])
+            ixl1.set_xdata([0, 0])
+            ixl1.set_ydata([0, 0])
     else:
         # Large values outside plot
         path.set_xdata([200, 200])
